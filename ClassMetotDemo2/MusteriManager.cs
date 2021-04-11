d45f98abec58f9c11c77042cb5415425f8449c11
Fazilabi <fazilag@code.edu.az>
2021-04-11 09:23:17 +0400

    Add project files.

---
+++ ClassMetotDemo2/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo2
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " -> Eklendi");
+
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " -> Silindi");
+
+        }
+        public void Guncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " -> Guncellendi");
+
+        }
+        public void Goruntule(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " -> Goruntulendi");
+
+        }
+    }
+}
---
+++ ClassMetotDemo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassMetotDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri() { Adi = "Ali", Soyadi = "Aliyev", Shehir = "Baku", TCNo = "123456" };
+            Musteri musteri2 = new Musteri() { Adi = "Veli", Soyadi = "Veliyev", Shehir = "Adana", TCNo = "24682468" };
+            Musteri musteri3 = new Musteri() { Adi = "Ahmet", Soyadi = "Ahmedov", Shehir = "Sheki", TCNo = "13579135" };
+            Musteri musteri4 = new Musteri() { Adi = "Mehmet", Soyadi = "Memmedov", Shehir = "Quba", TCNo = "321654" };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri3);
+            musteriManager.Guncelle(musteri2);
+            musteriManager.Sil(musteri4);
+            musteriManager.Goruntule(musteri1);
+        }
+    }
+}
